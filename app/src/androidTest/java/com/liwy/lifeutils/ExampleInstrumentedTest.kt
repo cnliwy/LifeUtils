eea54829ebce35ed5a852f8ac4a68ed6203b4b17
cnliwy <cnliwy@163.com>
2017-11-20 16:11:03 +0800

    init

---
+++ app/src/androidTest/java/com/liwy/lifeutils/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.liwy.lifeutils
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.liwy.lifeutils", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/com/liwy/lifeutils/entity/Menu.kt
@@ -0,0 +1,10 @@
+package com.liwy.lifeutils.entity
+
+/**
+ * Created by liwy on 2017/11/20.
+ */
+class Menu(name:String){
+    var name:String?=name
+    var id:String? = null
+    var logo:String?=null
+}
